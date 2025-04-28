2bcde3937dea1dc984b23ebe0699c1dd892f423c
Kethesh <136682936+Kethesh@users.noreply.github.com>
2025-04-28 21:31:50 +0530

    app.py

--- app.py
+++ app.py
@@ -112,5 +112,7 @@ def predict():
         return jsonify({"error": "Internal Server Error", "details": str(e)}), 500
 
 # === Run Flask App ===
+
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))  # Get port from Render or default to 5000
+    app.run(host="0.0.0.0", port=port)
